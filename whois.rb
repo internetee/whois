de6fd948d646dc49d8516983c45669e2881c4a8e
Priit Tark <priit@gitlab.eu>
2015-04-28 18:49:04 +0300

    Name whois server

--- whois.rb
+++ whois.rb
@@ -1,22 +1,11 @@
 require 'bundler/setup'
 require 'daemons'
-require 'active_record'
-require 'syslog/logger'
-
-def logger
-  @logger ||= Syslog::Logger.new 'whois'
-end
 
 root_path  = File.expand_path('.')
 whois_server = root_path + '/lib/whois_server.rb'
 
-if ENV['WHOIS_ENV'].nil? 
-  WHOIS_ENV = 'development'
-else
-  WHOIS_ENV = 'production'
-end
-
 Daemons.run(whois_server,
+  app_name: 'whois_server',
   log_output: true,
   output_logfilename: 'whois.log',
   logfilename: 'whois.log',
