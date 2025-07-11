8ea51c110300291d0f16cbf24935b1a866ee28eb
Rait Nigol <rait@tld.ee>
2025-07-11 10:32:07 +0300

    adjust WHOIS regex to allow 1-letter second-level domains (SLD) in WHOIS queries

--- lib/whois_server.rb
+++ lib/whois_server.rb
@@ -24,8 +24,8 @@ end
 module WhoisServer
   include Logging
 
-  DOMAIN_NAME_REGEXP = /\A[a-z0-9\-\u00E4\u00F5\u00F6\u00FC\u0161\u017E]{2,61}\.
-    ([a-z0-9\-\u00E4\u00F5\u00F6\u00FC\u0161\u017E]{2,61}\.)?[a-z0-9]{1,61}\z/x.freeze
+  DOMAIN_NAME_REGEXP = /\A[a-z0-9\-\u00E4\u00F5\u00F6\u00FC\u0161\u017E]{1,61}\.
+    ([a-z0-9\-\u00E4\u00F5\u00F6\u00FC\u0161\u017E]{1,61}\.)?[a-z0-9]{1,61}\z/x.freeze
 
   def dbconfig
     return @dbconfig unless @dbconfig.nil?
